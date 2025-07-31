b8c8efbaf371de9f6a791c06c881f18f873d7ab7
Amol Shirke <shirkeamy@gmail.com>
2025-07-31 10:34:13 +0530

    3. How to install any package into react

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,31 @@ import React from 'react';
 function App() {
   const userName: string = "Amol Shirke";
   {/*
-      How to write large HTML code in React
+      How to install any package into react
     
-      1. Multiple tag not allowed in React Components
-      2. We must have to use one top level tag. Any tag can be used.
-      3. Alternative is we can use fragment to wrap multiple tags <></>
-      4. If any element/tag is not closed properly then JSX will throw an error.
-      5. If we want to use class in JSX element the we have to use className attribute
-      We can not use below code directly in react
+      If we want to use any library/package into react we have to install it.
+      To install library/package, npm help us to install the library/package.
+      We can install using 2 ways
+      1. using npm in+
+      stall command
+      2. by adding package into package.json file
 
-      <h1>Welcome, {userName} to React + Typescript Tutorial</h1>
-      <p>
-        This is paragraph and second HTML tag in this file
-      </p>
-      
-      If we write this type of code in React we got the error as 
-      "TS2657: JSX expressions must have one parent element."
-      
-      and to resolve this error 
-      we have use parent tag for those both tab like div at a top or we can use fragment.
-      Fragment is the best practice to use as top level element
+      before using any of this step we have to identify the package and
+      it version for installtions.
+      we can foind any of the package on official site of npm "https://www.npmjs.com/"
+      we have to search the package which we want to install into our application like bootstrap
 
+      If we want to install bootstrap into our application we can serach bootstrap on npm's web site
+      Then we can select the package from the search result
+      and we can see the installation command at right hand side
+      "npm i bootstrap"
+      just copy it and paste is into the terminal and hit enter
+      This will install bootstrap and we able to see the entry will appear into package.json
+      file as well.
+      and we can do the same installation from package json file as well
+      we just need to add package name with it's version into package.json file and
+      run "npm install" command into terminal
+      "bootstrap": "5.3.7"
   */}
   return (
     <>
