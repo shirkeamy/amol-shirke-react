e97a2626cccbe69c13d1474b4b9c0bc0f4127d66
Amol Shirke <shirkeamy@gmail.com>
2025-07-31 09:09:39 +0530

    1. What is JSX in React?

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function App() {
+  const userName: string = "Amol Shirke";
   return (
     <>
-      <h1>Welcome to React Typescript Tutorial</h1>
+    {/*
+      JSX – JavaScript XML
+      Allow us to write HTML code in React
+      We can code something in between HTML code using {}
+    */}
+
+      <h1>Welcome, {userName} to React + Typescript Tutorial</h1>
+
     </>
   );
 }
